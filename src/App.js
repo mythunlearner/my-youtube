387de6644fce4ee7431616ab4ac8f6b8d18ae4f3
mythunlearner <learntechpassion@gmail.com>
2025-02-14 11:02:17 +0530

    Base Code commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Head from './components/Head';
+import Body from './components/Body';
+
+function App() {
+  return (
+    <div>
+         <Head/>
+         <Body/>
+         {
+          /**
+           * Head
+           * Body
+           *  Sidebar
+           *    MenuItems
+           *  MainContainer
+           *     ButtonList
+           *     VideoContainer
+           *       VideoCard
+           */
+         }
+    </div>
+  );
+}
+
+export default App;
